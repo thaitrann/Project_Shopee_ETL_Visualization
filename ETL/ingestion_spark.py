d753777a35d08b83753b7e679cbc9c072b81e856
Thaitran <tranthai6693@gmail.com>
2023-07-20 00:18:40 +0700

    done ingestion to mongodb to hdfs with pyspark

--- ETL/ingestion_spark.py
+++ ETL/ingestion_spark.py
@@ -1,41 +1,37 @@
-import os
-import findspark
-findspark.init("C:\spark-3.2.4-bin-hadoop3.2")
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import to_timestamp, date_format
-from pyspark.sql.functions import col
-import time
-start_time = time.time()
+from setup import *
 
-#config
-os.environ["SPARK_LOCAL_IP"] = "127.0.0.1"
-spark = SparkSession.builder.appName('Ingestion - from MONGODB to HDFS').\
-    config("spark.driver.bindAddress","localhost").\
-    config("spark.ui.port","4040").\
-    config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1").\
-    getOrCreate()
-spark.conf.set("spark.sql.legacy.timeParserPolicy", "LEGACY")
-spark.conf.set("spark.rpc.maxmessagesize", "10000000")
-spark.sparkContext.setLogLevel("off")
-sc = spark.sparkContext
+collections = ['collection_products_serp', 'collection_products_detail']
+def Ingestion(collections):
+    for collection in collections:
+        uri = "mongodb://localhost:27017/products_tiki.{}".format(collection)
 
-URI = sc._gateway.jvm.java.net.URI
-Path = sc._gateway.jvm.org.apache.hadoop.fs.Path 
-FileSystem = sc._gateway.jvm.org.apache.hadoop.fs.FileSystem
-Configuration = sc._gateway.jvm.org.apache.hadoop.conf.Configuration
+        #get the latest record in datalake 
+        fs = FileSystem.get(URI("hdfs://localhost:19000"), Configuration())
+        exists = fs.exists(Path('/datalake/{}'.format(collection)))
+        location_datalake = 'hdfs://localhost:19000/datalake/{}'.format(collection)
+        if exists:
+            df_datalake = spark.read.parquet(location_datalake)
+            max_date_df_datalake = df_datalake.select(F.max(F.col("completion_time"))).rdd.first()[0]
+        else:
+            max_date_df_datalake = datetime.datetime.fromtimestamp(0)
 
-collection = 'collection_products_serp'
-uri = "mongodb://localhost:27017/products_tiki.{}".format(collection)
-df = spark.read.format("mongo").option("uri", uri).load()
+        #get the latest record in mongodb 
+        mongo_df = spark.read.format("mongo").option("uri", uri).load()
+        output_mongo_df = mongo_df.filter(mongo_df["completion_time"] > max_date_df_datalake)
+        output_mongo_df = output_mongo_df.\
+            withColumn("completion_year", date_format(col("completion_time"), "Y")).\
+            withColumn("completion_month", date_format(col("completion_time"), "M")).\
+            withColumn("completion_day", date_format(col("completion_time"), "d"))  
+        print('--- Ingestion to table: {} ---'.format(collection))
+        print('--- Number of lines to ingest: {} ---'.format(output_mongo_df.count()))
 
-# Print the data
-df = df.withColumn("completion_time", to_timestamp(col("completion_time"))).\
-    withColumn("completion_year", date_format(col("completion_time"), "Y")).\
-    withColumn("completion_month", date_format(col("completion_time"), "M")).\
-    withColumn("completion_day", date_format(col("completion_time"), "d"))
-    
-df.select('completion_time', 'completion_year', 'completion_month', 'completion_day').show()
-fs = FileSystem.get(URI("hdfs://localhost:19000"), Configuration())
-print(fs.exists(Path('/datalake')))
+        #ingest to datalake
+        output_mongo_df.write.partitionBy('completion_year', 'completion_month', 'completion_day')\
+            .mode('append')\
+            .parquet(location_datalake)
 
-print(f"Execution time: {time.time() - start_time}")
+        print("--- Done Ingestion! ---")
+        print(f"Execution time: {time.time() - start_time}")
+        print("--------------------------------------")
+
+Ingestion(collections)
---
+++ ETL/setup.py
@@ -0,0 +1,26 @@
+import os
+import findspark
+findspark.init("C:\spark-3.2.4-bin-hadoop3.2")
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import date_format
+from pyspark.sql.functions import col
+import time
+start_time = time.time()
+import pyspark.sql.functions as F
+import datetime
+
+#config
+os.environ["SPARK_LOCAL_IP"] = "127.0.0.1"
+spark = SparkSession.builder.appName('Ingestion - from MONGODB to HDFS').\
+    config("spark.driver.bindAddress","localhost").\
+    config("spark.ui.port","4040").\
+    config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1").\
+    getOrCreate()
+spark.conf.set("spark.sql.legacy.timeParserPolicy", "LEGACY")
+spark.sparkContext.setLogLevel("off")
+sc = spark.sparkContext
+
+URI = sc._gateway.jvm.java.net.URI
+Path = sc._gateway.jvm.org.apache.hadoop.fs.Path 
+FileSystem = sc._gateway.jvm.org.apache.hadoop.fs.FileSystem
+Configuration = sc._gateway.jvm.org.apache.hadoop.conf.Configuration
